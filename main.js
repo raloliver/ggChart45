eae9bc2294a37a6a7114ecb541d5a7ef5952533f
Ral Oliver <raloliver.ws@gmail.com>
2017-04-25 00:44:37 -0300

    Add Items #2

--- main.js
+++ main.js
@@ -1,34 +1,81 @@
 /// <reference path="google-chart.d.ts" />
 
-google.charts.load('current', {packages: ['corechart']});
+google.charts.load('current', {
+    packages: ['corechart']
+});
 
-google.charts.setOnLoadCallback(desenhaGrafico);
+google.charts.setOnLoadCallback(iniciarGG);
 
-function desenhaGrafico() {   
-    
+function iniciarGG() {
+    desenhaGrafico();
+    let btnInserir = document.querySelector('#btn-inserir');
+    let btnAtualizar = document.querySelector('#btn-atualizar');
+    //ao carregar o GG, habilitar o botão
+    btnInserir.removeAttribute('disabled');
+    btnAtualizar.removeAttribute('disabled');
+}
+
+function desenhaGrafico() {
+
+    //pegando os dados de uma tabela
+    let data = inserirGrafico();
+
+    /*
+    //bando de dados burro
     let data = new google.visualization.DataTable();
+    
     //adicionando colunas: qual o tipo e o nome?
-    data.addColumn('string', 'Vendas');
-    data.addColumn('number', 'Total Diário');
+    data.addColumn('string', 'Dia da Semana');
+    data.addColumn('number', 'Total');
 
     //adicionando array de linhas
-    data.addRows([
-        ['Segunda', 40],
-        ['Terça', 32],
-        ['Quarta', 24],
-        ['Quinta', 18],
-        ['Sexta', 13],
-        ['Sábado', 7],
+    data.addRows([        
         ['Domingo', 4]
     ]);
+    */
 
     let options = {
         title: 'Qual dia você trabalhou mais?',
         width: 720,
-        height: 480
+        height: 360
     }
 
     let graficoVendas = new google.visualization.PieChart(document.querySelector('#grafico-vendas'));
 
     graficoVendas.draw(data, options);
+}
+
+function inserirDados(evento) {
+    evento.preventDefault();
+    let date = document.querySelector('#date').value;
+    let sales = document.querySelector('#sales').value;
+    let table = document.querySelector('.table-striped');
+    let rows = table.querySelectorAll('tbody')[0].children.length;
+    let row = table.insertRow(rows + 1);
+
+    let cellA = row.insertCell(0);
+    cellA.innerHTML = date;
+    let cellB = row.insertCell(1);
+    cellB.innerHTML = sales;
+}
+
+function inserirGrafico() {
+    //coletando os dados
+    let table = document.querySelector('.table-striped');
+    let rows = table.querySelectorAll('tbody')[0].children;
+    let data = new google.visualization.DataTable();
+
+    //adicionando colunas: qual o tipo e o nome?
+    data.addColumn('string', 'Dia da Semana');
+    data.addColumn('number', 'Total');
+
+    for (let index = 0; index < rows.length; index++) {
+        let date = rows[index].cells[0].innerHTML;
+        //converter para número para dizer ao GG o formato correto
+        let sales = parseInt(rows[index].cells[1].innerHTML);
+        //GG add linhas
+        data.addRow([date, sales]);
+    }
+    console.log(data)
+    return data;
 }
